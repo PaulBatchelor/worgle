a3cb9c0d604e5affd58c4fd58d33c000c748fd6e
Paul Batchelor <thisispaulbatchelor@gmail.com>
2018-10-06 19:52:01 -0400

    sorg benchmarking added

--- measure_performance.sh
+++ measure_performance.sh
@@ -7,3 +7,13 @@ printf "Org-babel-tangle (via Emacs): %s\n" $TIME
 
 TIME=$((time ./worgle -g worgle.org) 2>&1 | grep "user" | cut -f 2)
 printf "Worgle: %s\n" $TIME
+
+TIME=$((time emacs --batch -f package-initialize -l org \
+    --eval '(find-file "worgle.org")' \
+    --eval '(org-html-export-to-html)') 2>&1 |\
+    grep "user" | cut -f 2)
+
+printf "Org-export-to-html (via Emacs): %s\n" $TIME
+
+TIME=$((time ./sorg < worgle.org > worgle.html) 2>&1 | grep "user" | cut -f 2)
+printf "Sorg: %s\n" $TIME
